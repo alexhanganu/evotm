bf8dbe16e981d61e7d1f4ccce4a40c1e7101b7d2
alexhanganu <alexandru@hanganu.ca>
2020-12-09 12:04:28 -0500

    Fix name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tm",
+    name="evotm",
     version="0.0.1",
     author="Alexandru Hanganu",
     author_email="alexandru.hanganu@umontreal.ca",
     description="Time and Task management app with Google Calendar link",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alexhanganu/tm",
+    url="https://github.com/alexhanganu/evotm",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
